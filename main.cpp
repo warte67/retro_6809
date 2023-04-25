fa8a69f76df87b5fef35ad7d765c33bce7cef4e6
Warte67 <jayfaries@yahoo.com>
2023-04-25 12:17:53 -0700

    Initial Commits

--- main.cpp
+++ main.cpp
@@ -2,39 +2,16 @@
 main.cpp
 
 build with debug symbols:
-g++ -g *.cpp -o bin/main -std=c++20 -lSDL2 
+    g++ -g *.cpp -o bin/main -std=c++20 -lSDL2 
 
 build without debug symbols:
-g++ *.cpp -o bin/main -std=c++20 -lSDL2 
+    g++ *.cpp -o bin/main -std=c++20 -lSDL2 
 
-run:
-./bin/main
-
-
-GIT:
-https://www.youtube.com/watch?v=gJv0PcfUXE8
-
-<this folder>
-git init
-git config --global user.name "Warte67"
-git config --global user.email "jayfaries@yahoo.com"
-
-git status
-git add .
-git commit -m "first commit"
-
-git log --oneline
-git revert <hash>
-
-git remote add origin https://github.com/warte67/retro_6809.git
-
-
-=============== TESTING WORKED ===============
-git status
-git add .
-git commit -m "first commit"
-
-git push origin master
+GIT Stuff:
+    git status
+    git add .
+    git commit -m "Commit Comment"
+    git push origin master
 
 *****************************************************/
 
