be279be2b12e05d9b5fff546adcedb3fa3690b22
Warte67 <jayfaries@yahoo.com>
2023-04-26 18:49:06 -0700

    Testing

--- Gfx.h
+++ Gfx.h
@@ -29,7 +29,7 @@ class Gfx : public Device
         virtual Byte read(Word offset, bool debug = false);
         virtual void write(Word offset, Byte data, bool debug = false);
 
-        // accessors
+        // read-only accessors:
         SDL_Renderer* GetRenderer() { return _renderer; }
         SDL_Window* GetWindow() { return _window; }
         SDL_Texture* GetBGTexture() { return _bg_texture; }
@@ -65,20 +65,68 @@ class Gfx : public Device
         bool _fullscreen = false;
         bool _vsync = false;
 
+        // base graphics device registers
         Byte _dsp_gmode = 0x00;     // DSP_GMODE
+        Byte _dsp_emuflags = 0;     // DSP_EMUFLAGS
         Byte _dsp_pxlofs = 0;       // DSP_PXLOFS
         Word _dsp_tbase = 0x0400;   // DSP_TBASE
         Word _dsp_gbase = 0x0000;   // DSP_GBASE
         Word _dsp_gaddr = 0x0000;   // DSP_GADDR
         Byte _dsp_data = 0x00;      // DSP_GDATA
+        // display pitch registers (not yet implemented)
+        Word _dsp_tpitch = _texture_width / 8;  // DSP_TPITCH
+        Word _dsp_gpitch = _texture_width;      // DSP_GPITCH
+
+        // color palette
         Byte _dsp_pal_idx = 0x00;   // DSP_PAL_IDX
         Word _dsp_pal_clr = 0x0000; // DSP_PAL_CLR
+        // text glyph definition array  (move to glyph mode device)
         Byte _dsp_glyph_idx = 0x00; // DSP_GLYPH_IDX
-        // Byte _dsp_glhph_data[8]{0}; // DSP_GLYPH_DATA   *
-        Byte _dsp_emuflags = 0;     // DSP_EMUFLAGS
+        // Byte _dsp_glhph_data[8]{0}; // DSP_GLYPH_DATA 
 
+
+        // 64k graphics display buffer
+        std::vector<Byte> _gfxDisplayBuffer;
+
+        // helper functions:
+        void _updateGraphics(float fElapsedTime);
+        void _updateTiles(float fElapsedTime);
+        void _updateTextGlyphs(float fElapsedTime);
+        void _updateSprites(float fElapsedTime);
+
+        // Palette Stuff
+        union PALETTE {
+            Word color;
+            struct {
+                Uint8 a : 4;
+                Uint8 b : 4;
+                Uint8 g : 4;
+                Uint8 r : 4;
+            };
+        };
+        std::vector<PALETTE> _palette;
     public:
-        // accessors:
+        Uint8 red(Uint8 index) { Uint8 c = _palette[index].r;  return c | (c << 4) | (c << 8) | (c << 12); }
+        Uint8 grn(Uint8 index) { Uint8 c = _palette[index].g;  return c | (c << 4) | (c << 8) | (c << 12); }
+        Uint8 blu(Uint8 index) { Uint8 c = _palette[index].b;  return c | (c << 4) | (c << 8) | (c << 12); }
+        Uint8 alf(Uint8 index) { Uint8 c = _palette[index].a;  return c | (c << 4) | (c << 8) | (c << 12); }        
 };
+
+
+
+
+
+
+
+//          // base class for the Graphics Modes
+//          class GfxNode : public Device
+//          {
+//              public:
+//                  GfxNode() {  Name("GfxNode"); }
+//                  GfxNode(std::string sName) { Name(sName); }
+//                  virtual ~GfxNode() {}           
+//                  virtual Word OnAttach(Word nextAddr) = 0;
+//          };
+
 
 #endif  // __GFX_H__
