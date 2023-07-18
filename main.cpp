f7c63ecc05c7aa5f388955d2ca0d3b1262b8c6d8
Warte67 <jayfaries@yahoo.com>
2023-07-17 20:16:31 -0700

    update

--- main.cpp
+++ main.cpp
@@ -34,6 +34,24 @@ int main(void)
 
 /**************************************
  
+ToDo NOTES:
+
+	1) To exponentially simplify the graphics  processing overhead, 
+		remove the 640x400 timing resolutions.
+
+	2) Debug device object shall run in its own (5k text buffer) in 1x timing
+
+	3) the mouse cursor itself shall run in 1x timing mode, however it will
+		by software scaleable
+
+	4) in the hardware version (2xPICO) all graphcis shall render to its own
+		50k render buffer.  Simulate this by rendering all graphics to a 
+		single render target texture for final presentation.
+
+	5) create a MakeFile to use incremental building with "Make".
+
+
+
 
 
 ***************************************/
