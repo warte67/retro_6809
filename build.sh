3a1940db09d789b97af4155dd07f3ff75eb88731
Warte67 <jayfaries@yahoo.com>
2023-10-28 02:02:18 -0700

    build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#lwasm -9 test.asm --format=ihex -otest.hex -ltest.lst
+#echo "lwasm -9 $1.asm --format=ihex -o$1.hex -l$1.lst"
+#lwasm -9 $1.asm --format=ihex -o$1.hex -l$1.lst
+
+
+echo "asm6809 -H -9 $1.asm -o $1.hex -l $1.lst"
+cd asm
+asm6809 -H -9 $1.asm -o $1.hex -l $1.lst
+cd ..
