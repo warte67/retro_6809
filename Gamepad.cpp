e554ab5f5ecce632315cf1b0bf240817f6798fcb
Jay Faries <121575921+warte67@users.noreply.github.com>
2023-11-08 10:00:33 -0800

    Gamepad object working

--- Gamepad.cpp
+++ Gamepad.cpp
@@ -1,6 +1,11 @@
 /**** Gamepad.cpp ***************************************
  *
  * The Gamepad / Joystick device interface.
+ * 
+ *		Notes:
+ *			Tested specifically with Logitech F310 gamepad controllers
+ *			and Extreme 3D Pro joysticks. Other controller devices will
+ *			likely work, but have not yet been tested.
  *
  * Copyright (C) 2023 by Jay Faries
  ************************************/
@@ -10,13 +15,7 @@
 
 Byte Gamepad::read(Word offset, bool debug)
 {
-    Byte data = 0xCC;
-
-    switch (offset)
-    {
-    default:    break;
-    }
-
+	Byte data = Bus::Read(offset, true);
     // write statically and return
     Bus::Write(offset, data, true);
     return data;
@@ -24,12 +23,7 @@ Byte Gamepad::read(Word offset, bool debug)
 
 void Gamepad::write(Word offset, Byte data, bool debug)
 {
-    switch (offset)
-    {
-    default:  break;
-    }
-
-    // write statically
+	// simply write statically
     Bus::Write(offset, data, true);
 }
 
@@ -87,30 +81,298 @@ Word Gamepad::OnAttach(Word nextAddr)
 void Gamepad::OnInit()
 {
     // printf("Gamepad::OnInit();\n");
+	InitButtonStates();
+	OnActivate();
 }
 
 void Gamepad::OnQuit()
 {
     // printf("Gamepad::OnQuit();\n");
+	OnDeactivate();
 }
 
 void Gamepad::OnActivate()
 {
     // printf("Gamepad::OnActivate();\n");
+	if (!bGamepadWasInit)
+	{
+		int ret = SDL_InitSubSystem(SDL_INIT_JOYSTICK | SDL_INIT_GAMECONTROLLER);
+		if (ret < 0)
+		{
+			std::string msg = "SDL could not initialize gamepads and/or joysticks! SDL_Error: ";
+			msg += SDL_GetError();
+			Bus::Error(msg.c_str());
+			return;
+		}
+		// set the default analog dead band (dead zone)
+		Bus::Write(JOYS_1_DBND, 5, true);	// bus->debug_write(JOYS_1_DBND, 5);
+		Bus::Write(JOYS_2_DBND, 5, true);	// bus->debug_write(JOYS_2_DBND, 5);
+		OpenControllers();
+		bGamepadWasInit = true;
+	}
 }
 
 void Gamepad::OnDeactivate()
 {
     // printf("Gamepad::OnDeactivate();\n");
+	if (bGamepadWasInit)
+	{
+		CloseControllers();
+		SDL_QuitSubSystem(SDL_INIT_JOYSTICK | SDL_INIT_GAMECONTROLLER);
+		bGamepadWasInit = false;
+	}
 }
 
 void Gamepad::OnEvent(SDL_Event* evnt)
 {
+    switch (evnt->type)
+    {
+    case SDL_CONTROLLERDEVICEADDED:
+    case SDL_CONTROLLERDEVICEREMOVED:
+    case SDL_CONTROLLERDEVICEREMAPPED:
+    case SDL_JOYDEVICEADDED:
+    case SDL_JOYDEVICEREMOVED:
+        CloseControllers();
+        OpenControllers();
+        break;
+    }
 }
 
 void Gamepad::OnUpdate(float fElapsedTime)
 {
+	// update button registers
+	if (state[0].bIsActive)
+		Bus::Write_Word(JOYS_1_BTN, EncodeButtonRegister(0), true);
+	else
+		Bus::Write_Word(JOYS_1_BTN, 0xffff, true);
+	if (state[1].bIsActive)
+		Bus::Write_Word(JOYS_2_BTN, EncodeButtonRegister(1), true);
+	else
+		Bus::Write_Word(JOYS_2_BTN, 0xffff, true);
+	EncodeAxesRegister(0);
+	EncodeAxesRegister(1);
 }
 
+
+
+// ******************************************************************
+
+void Gamepad::InitButtonStates()
+{
+	Byte btn = 0;
+	gpadBtnMap.clear();
+	joysBtnMap.clear();
+
+	gpadBtnMap[SDL_CONTROLLER_BUTTON_A] = btn++;			// GPAD_BTN::BTN_A
+	gpadBtnMap[SDL_CONTROLLER_BUTTON_B] = btn++;			// GPAD_BTN::BTN_B
+	gpadBtnMap[SDL_CONTROLLER_BUTTON_X] = btn++;			// GPAD_BTN::BTN_X
+	gpadBtnMap[SDL_CONTROLLER_BUTTON_Y] = btn++;			// GPAD_BTN::BTN_Y
+	gpadBtnMap[SDL_CONTROLLER_BUTTON_BACK] = btn++;			// GPAD_BTN::BTN_BACK
+	gpadBtnMap[SDL_CONTROLLER_BUTTON_START] = btn++;		// GPAD_BTN::BTN_START
+	gpadBtnMap[SDL_CONTROLLER_BUTTON_LEFTSHOULDER] = btn++;	// GPAD_BTN::BTN_LS
+	gpadBtnMap[SDL_CONTROLLER_BUTTON_RIGHTSHOULDER] = btn++;	// GPAD_BTN::BTN_RS
+	gpadBtnMap[SDL_CONTROLLER_BUTTON_DPAD_UP] = btn++;		// GPAD_BTN::BTN_DPAD_UP
+	gpadBtnMap[SDL_CONTROLLER_BUTTON_DPAD_DOWN] = btn++;	// GPAD_BTN::BTN_DPAD_DOWN
+	gpadBtnMap[SDL_CONTROLLER_BUTTON_DPAD_LEFT] = btn++;	// GPAD_BTN::BTN_DPAD_LEFT
+	gpadBtnMap[SDL_CONTROLLER_BUTTON_DPAD_RIGHT] = btn++;	// GPAD_BTN::BTN_DPAD_RIGHT
+	gpadBtnMap[SDL_CONTROLLER_BUTTON_GUIDE] = btn++;		// GPAD_BTN::BTN_GUIDE
+
+	btn = 0;
+	joysBtnMap[JOYS_BTN::BTN_1] = btn++;
+	joysBtnMap[JOYS_BTN::BTN_2] = btn++;
+	joysBtnMap[JOYS_BTN::BTN_3] = btn++;
+	joysBtnMap[JOYS_BTN::BTN_4] = btn++;
+	joysBtnMap[JOYS_BTN::BTN_5] = btn++;
+	joysBtnMap[JOYS_BTN::BTN_6] = btn++;
+	joysBtnMap[JOYS_BTN::BTN_7] = btn++;
+	joysBtnMap[JOYS_BTN::BTN_8] = btn++;
+	joysBtnMap[JOYS_BTN::BTN_HAT_UP] = btn++;
+	joysBtnMap[JOYS_BTN::BTN_HAT_DOWN] = btn++;
+	joysBtnMap[JOYS_BTN::BTN_HAT_LEFT] = btn++;
+	joysBtnMap[JOYS_BTN::BTN_HAT_RIGHT] = btn++;
+	joysBtnMap[JOYS_BTN::BTN_9] = btn++;
+	joysBtnMap[JOYS_BTN::BTN_10] = btn++;
+	joysBtnMap[JOYS_BTN::BTN_11] = btn++;
+	joysBtnMap[JOYS_BTN::BTN_12] = btn++;
+}
+
+
+void Gamepad::OpenControllers()
+{
+	int num_joysticks = SDL_NumJoysticks();
+	for (int index = 0; index < num_joysticks; index++)
+	{
+		if (SDL_IsGameController(index)) // is a gamepad
+		{
+			state[index].bIsGamepad = true;
+			state[index].name = SDL_GameControllerNameForIndex(index);
+			state[index].controller = SDL_GameControllerOpen(index);
+			if (state[index].controller == nullptr)
+			{
+				std::string msg = "SDL could not open a gamepad! SDL_Error: ";
+				msg += SDL_GetError();
+				Bus::Error(msg.c_str());
+				return;
+			}
+			state[index].bIsActive = true;
+		}
+		else // is a joystick
+		{
+			state[index].bIsGamepad = false;
+			state[index].joystick = SDL_JoystickOpen(index);
+			if (state[index].joystick == nullptr)
+			{
+				std::string msg = "SDL could not open a joystick! SDL_Error: ";
+				msg += SDL_GetError();
+				Bus::Error(msg.c_str());
+				return;
+			}
+			state[index].name = SDL_JoystickName(state[index].joystick);
+			state[index].bIsActive = true;
+		}
+	}
+}
+
+void Gamepad::CloseControllers()
+{
+	state[0].bIsActive = false;
+	Bus::Write_Word(JOYS_1_BTN, 0xffff, true);	// bus->debug_write_word(JOYS_1_BTN, 0xffff);
+	state[1].bIsActive = false;
+	Bus::Write_Word(JOYS_2_BTN, 0xffff, true);	// bus->debug_write_word(JOYS_2_BTN, 0xffff);
+}
+
+
+Word Gamepad::EncodeButtonRegister(int id)
+{
+	id %= 2;
+	Word ret = 0xffff;
+	if (state[id].bIsActive)
+	{
+		ret = 0;
+		// encode only the first 16 buttons
+		for (int btn = 0; btn < 16; btn++)
+		{
+			// gamepad?
+			if (state[id].bIsGamepad)
+			{
+				if (SDL_GameControllerGetButton(state[id].controller, (SDL_GameControllerButton)btn))
+					ret |= (1 << gpadBtnMap[btn]);
+			}
+			else // joystick
+			{
+				if (SDL_JoystickGetButton(state[id].joystick, btn))
+					ret |= (1 << joysBtnMap[btn]);
+				// encode hat
+				Byte hat = SDL_JoystickGetHat(state[id].joystick, 0);
+				if (hat == SDL_HAT_UP || hat == SDL_HAT_RIGHTUP || hat == SDL_HAT_LEFTUP)
+					ret |= 0x0100;
+				if (hat == SDL_HAT_DOWN || hat == SDL_HAT_RIGHTDOWN || hat == SDL_HAT_LEFTDOWN)
+					ret |= 0x0200;
+				if (hat == SDL_HAT_LEFT || hat == SDL_HAT_LEFTUP || hat == SDL_HAT_LEFTDOWN)
+					ret |= 0x0400;
+				if (hat == SDL_HAT_RIGHT || hat == SDL_HAT_RIGHTUP || hat == SDL_HAT_RIGHTDOWN)
+					ret |= 0x0800;
+			}
+		}
+	}
+	return ret;
+}
+
+
+void Gamepad::EncodeAxesRegister(int id)
+{
+	id %= 2;
+	if (state[id].bIsActive)
+	{
+		Sint8 deadband = read(JOYS_1_DBND);
+		if (id == 1)
+			Sint8 deadband = read(JOYS_2_DBND);
+
+		if (state[id].bIsGamepad)	// is gamepad
+		{
+			Sint8 LTX = (SDL_GameControllerGetAxis(state[id].controller, SDL_CONTROLLER_AXIS_LEFTX) & 0xff00) >> 8;
+			if (LTX < 0 && LTX > -deadband)	LTX = 0;
+			if (LTX > 0 && LTX < deadband)	LTX = 0;
+			Sint8 LTY = (SDL_GameControllerGetAxis(state[id].controller, SDL_CONTROLLER_AXIS_LEFTY) & 0xff00) >> 8;
+			if (LTY < 0 && LTY > -deadband)	LTY = 0;
+			if (LTY > 0 && LTY < deadband)	LTY = 0;
+			Sint8 RTX = (SDL_GameControllerGetAxis(state[id].controller, SDL_CONTROLLER_AXIS_RIGHTX) & 0xff00) >> 8;
+			if (RTX < 0 && RTX > -deadband)	RTX = 0;
+			if (RTX > 0 && RTX < deadband)	RTX = 0;
+			Sint8 RTY = (SDL_GameControllerGetAxis(state[id].controller, SDL_CONTROLLER_AXIS_RIGHTY) & 0xff00) >> 8;
+			if (RTY < 0 && RTY > -deadband)	RTY = 0;
+			if (RTY > 0 && RTY < deadband)	RTY = 0;
+			Sint8 Z1 = SDL_GameControllerGetAxis(state[id].controller, SDL_CONTROLLER_AXIS_TRIGGERLEFT) >> 8;
+			Sint8 Z2 = SDL_GameControllerGetAxis(state[id].controller, SDL_CONTROLLER_AXIS_TRIGGERRIGHT) >> 8;
+
+			if (id == 0)
+			{
+				Bus::Write(JOYS_1_LTX, LTX, true);	// bus->debug_write(JOYS_1_LTX, LTX);
+				Bus::Write(JOYS_1_LTY, LTY, true);	// bus->debug_write(JOYS_1_LTY, LTY);
+				Bus::Write(JOYS_1_RTX, RTX, true);	// bus->debug_write(JOYS_1_RTX, RTX);
+				Bus::Write(JOYS_1_RTY, RTY, true);	// bus->debug_write(JOYS_1_RTY, RTY);
+				Bus::Write(JOYS_1_Z1,   Z1, true);	// bus->debug_write(JOYS_1_Z1, Z1);
+				Bus::Write(JOYS_1_Z2,   Z2, true);	// bus->debug_write(JOYS_1_Z2, Z2);
+			}
+			else
+			{
+				Bus::Write(JOYS_2_LTX, LTX, true);	// bus->debug_write(JOYS_2_LTX, LTX);
+				Bus::Write(JOYS_2_LTY, LTY, true);	// bus->debug_write(JOYS_2_LTY, LTY);
+				Bus::Write(JOYS_2_RTX, RTX, true);	// bus->debug_write(JOYS_2_RTX, RTX);
+				Bus::Write(JOYS_2_RTY, RTY, true);	// bus->debug_write(JOYS_2_RTY, RTY);
+				Bus::Write(JOYS_2_Z1,   Z1, true);	// bus->debug_write(JOYS_2_Z1, Z1);
+				Bus::Write(JOYS_2_Z2,   Z2, true);	// bus->debug_write(JOYS_2_Z2, Z2);
+			}
+		}
+		else  // is joystick
+		{
+			Sint8 LTX = (SDL_JoystickGetAxis(state[id].joystick, 0) & 0xff00) >> 8;
+			if (LTX < 0 && LTX > -deadband)	LTX = 0;
+			if (LTX > 0 && LTX < deadband)	LTX = 0;
+			Sint8 LTY = (SDL_JoystickGetAxis(state[id].joystick, 1) & 0xff00) >> 8;
+			if (LTY < 0 && LTY > -deadband)	LTY = 0;
+			if (LTY > 0 && LTY < deadband)	LTY = 0;
+			// RTX maps the hat
+			Byte hat = SDL_JoystickGetHat(state[id].joystick, 0);
+			Sint8 RTX = 0;
+			Sint8 RTY = 0;
+			if (hat == SDL_HAT_UP || hat == SDL_HAT_RIGHTUP || hat == SDL_HAT_LEFTUP)
+				RTY = -128;
+			if (hat == SDL_HAT_DOWN || hat == SDL_HAT_RIGHTDOWN || hat == SDL_HAT_LEFTDOWN)
+				RTY = 127;
+			if (hat == SDL_HAT_LEFT || hat == SDL_HAT_LEFTUP || hat == SDL_HAT_LEFTDOWN)
+				RTX = -128;
+			if (hat == SDL_HAT_RIGHT || hat == SDL_HAT_RIGHTUP || hat == SDL_HAT_RIGHTDOWN)
+				RTX = 127;
+			// throttle lever
+			Uint8 Z1 = SDL_JoystickGetAxis(state[id].joystick, 3) >> 8;
+			Z1 = 255 - (Z1 + 128);
+
+			// rudder axis
+			Sint8 Z2 = SDL_JoystickGetAxis(state[id].joystick, 2) >> 8;
+			if (Z2 < 0 && Z2 > -deadband * 3)	Z2 = 0;
+			if (Z2 > 0 && Z2 < deadband * 3)	Z2 = 0;
+
+			if (id == 0)
+			{
+				Bus::Write(JOYS_1_LTX, LTX, true);	// bus->debug_write(JOYS_1_LTX, LTX);
+				Bus::Write(JOYS_1_LTY, LTY, true);	// bus->debug_write(JOYS_1_LTY, LTY);
+				Bus::Write(JOYS_1_RTX, RTX, true);	// bus->debug_write(JOYS_1_RTX, RTX);
+				Bus::Write(JOYS_1_RTY, RTY, true);	// bus->debug_write(JOYS_1_RTY, RTY);
+				Bus::Write(JOYS_1_Z1,   Z1, true);	// bus->debug_write(JOYS_1_Z1, Z1);
+				Bus::Write(JOYS_1_Z2,   Z2, true);	// bus->debug_write(JOYS_1_Z2, Z2);
+			}
+			else
+			{
+				Bus::Write(JOYS_2_LTX, LTX, true);	// bus->debug_write(JOYS_2_LTX, LTX);
+				Bus::Write(JOYS_2_LTY, LTY, true);	// bus->debug_write(JOYS_2_LTY, LTY);
+				Bus::Write(JOYS_2_RTX, RTX, true);	// bus->debug_write(JOYS_2_RTX, RTX);
+				Bus::Write(JOYS_2_RTY, RTY, true);	// bus->debug_write(JOYS_2_RTY, RTY);
+				Bus::Write(JOYS_2_Z1, Z1, true);	// bus->debug_write(JOYS_2_Z1, Z1);
+				Bus::Write(JOYS_2_Z2, Z2, true);	// bus->debug_write(JOYS_2_Z2, Z2);
+			}
+		}
+	}
+}
 
 
--- Gamepad.h
+++ Gamepad.h
@@ -35,16 +35,70 @@ public:
     virtual void OnDeactivate() override;
     virtual void OnEvent(SDL_Event* evnt) override;
     virtual void OnUpdate(float fElapsedTime) override;
-    // virtual void OnRender() override;
 
     virtual Byte read(Word offset, bool debug = false);
     virtual void write(Word offset, Byte data, bool debug = false);
-    // virtual Word read_word(Word offset, bool debug = false);
-    // virtual void write_word(Word offset, Word data, bool debug = false);
 
-    // helpers
-    // virtual void DisplayEnum(std::string sToken, Word ofs, std::string sComment);
+protected:
+    void OpenControllers();
+    void CloseControllers();
+    void InitButtonStates();
+    Word EncodeButtonRegister(int id);
+    void EncodeAxesRegister(int id);
 
+	struct STATE {
+		SDL_GameController* controller = nullptr;
+		SDL_Joystick* joystick = nullptr;
+		bool bIsActive = false;
+		bool bIsGamepad = false;
+		std::string name = "";
+	};
+
+	enum GPAD_BTN {
+		BTN_A,
+		BTN_B,
+		BTN_X,
+		BTN_Y,
+		BTN_BACK,
+		BTN_START,
+		BTN_LS,
+		BTN_RS,
+		BTN_DPAD_UP,
+		BTN_DPAD_DOWN,
+		BTN_DPAD_LEFT,
+		BTN_DPAD_RIGHT,
+		BTN_GUIDE,
+	};
+
+	enum JOYS_BTN {
+		BTN_1,
+		BTN_2,
+		BTN_3,
+		BTN_4,
+		BTN_5,
+		BTN_6,
+		BTN_7,
+		BTN_8,
+		BTN_9,
+		BTN_10,
+		BTN_11,
+		BTN_12,
+		BTN_HAT_UP,
+		BTN_HAT_DOWN,
+		BTN_HAT_LEFT,
+		BTN_HAT_RIGHT,
+	};
+
+
+private:
+
+	// Bus* bus = nullptr;
+	// Memory* memory = nullptr;
+
+	bool bGamepadWasInit = false;
+	STATE state[2];
+	std::map<Word, Byte> joysBtnMap;
+	std::map<Word, Byte> gpadBtnMap;
 };
 
 
