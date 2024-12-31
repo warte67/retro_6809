3467a062a85722739c36ad3dea94eed9647edf18
warte67 <jayfaries@yahoo.com>
2024-12-30 21:48:08 -0800

    Update GPU.hpp

--- include/GPU.hpp
+++ include/GPU.hpp
@@ -79,6 +79,113 @@ private: // PRIVATE MEMBERS
     2-bits for Hscan
     1-bit for text/bitmap mode
     1-bit reserved
+
+
+    SDL3 Pixel Format:  SDL_PIXELFORMAT_ARGB4444
+
+    see: https://wiki.libsdl.org/SDL3/SDL_PixelFormatDetails
+    see: https://wiki.libsdl.org/SDL3/SDL_CreateTexture
+
+    //Lock a portion of the texture for write-only pixel access.
+    SDL_LockTexture(texture, NULL, &pixels, &pitch);
+    //Unlock a texture, uploading the changes to video memory
+    SDL_UnlockTexture(texture);
+
+
+
+
+EXAMPLE:
+
+#include <SDL3/SDL.h>
+
+#include <stdlib.h>
+
+// Moving Rectangle 
+int main(int argc, char *argv[])
+{
+        SDL_Window *window;
+        SDL_Renderer *renderer;
+        SDL_Texture *texture;
+        SDL_Event event;
+        SDL_FRect r;
+
+        if (!SDL_Init(SDL_INIT_VIDEO)) {
+                SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
+                return 3;
+        }
+
+        window = SDL_CreateWindow("SDL_CreateTexture",
+                        1024, 768,
+                        SDL_WINDOW_RESIZABLE);
+
+        r.w = 100;
+        r.h = 50;
+
+        renderer = SDL_CreateRenderer(window, NULL);
+
+        texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 1024, 768);
+
+        while (1) {
+                SDL_PollEvent(&event);
+                if(event.type == SDL_EVENT_QUIT)
+                        break;
+                r.x=rand()%500;
+                r.y=rand()%500;
+
+                SDL_SetRenderTarget(renderer, texture);
+                SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+                SDL_RenderClear(renderer);
+                SDL_RenderRect(renderer,&r);
+                SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0x00);
+                SDL_RenderFillRect(renderer, &r);
+                SDL_SetRenderTarget(renderer, NULL);
+                SDL_RenderTexture(renderer, texture, NULL, NULL);
+                SDL_RenderPresent(renderer);
+        }
+        SDL_DestroyRenderer(renderer);
+        SDL_Quit();
+        return 0;
+}
+
+
+Also take a look at this example of rendering to a buffer then 
+copying it to a texture to be rendered.
+
+
+
+#include <SDL.h>
+
+SDL_Window *sdlWindow;
+SDL_Renderer *sdlRenderer;
+
+int main( int argc, char *args[] )
+{
+    int w = 640;
+    int h = 480;
+    Uint32 format = SDL_PIXELFORMAT_RGB888;
+    SDL_CreateWindowAndRenderer(w, h, 0, &sdlWindow, &sdlRenderer);
+    SDL_Texture *sdlTexture = SDL_CreateTexture(sdlRenderer, format, SDL_TEXTUREACCESS_STREAMING, w, h);
+    extern uint32_t *pixels;
+
+    for (int x = 0; x < w; x++) {
+        for (int y = 0; y < h; y++) {
+            pixels[x + y * w] = SDL_MapRGB(format, 255, 255, 255);
+        }
+    }
+
+    SDL_UpdateTexture(sdlTexture, NULL, pixels, 640 * sizeof (Uint32));
+    SDL_RenderClear(sdlRenderer);
+    SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, NULL);
+    SDL_RenderPresent(sdlRenderer);
+    SDL_Delay(5000);
+
+    SDL_Quit();
+    return 0;
+}
+
+
+
+
   
  ****************************************/
 
