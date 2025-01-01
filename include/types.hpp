3852547ef87cf447715d260cc911e85b1d322b9c
warte67 <jayfaries@yahoo.com>
2025-01-01 14:48:05 -0800

    Update types.hpp

--- include/types.hpp
+++ include/types.hpp
@@ -12,42 +12,42 @@
 
 // #pragma once    // (Easy Mode Header Guard)
 #ifndef __TYPES_HPP__
-#define __TYPES_HPP__
+    #define __TYPES_HPP__
 
-// GENERATE_MEMORY_MAP: Generate a memory map definition file?
-//      true:  generate Memory_Map.hpp and use the local unordered map
-//      false: to use enums from Memory_Map.hpp
-#define GENERATE_MEMORY_MAP     true         
+    // GENERATE_MEMORY_MAP: Generate a memory map definition file?
+    //      true:  generate Memory_Map.hpp and use the local unordered map
+    //      false: to use enums from Memory_Map.hpp
+    #define GENERATE_MEMORY_MAP     false         
 
-// Create an appropriate MAP() macro
-#if GENERATE_MEMORY_MAP == true
-    // use the unordered map
-    #define MAP_IMPL(key) Memory::Map(#key)                     
-    #define MAP(key) MAP_IMPL(key)
-#else // use the enumeration
-    #include "Memory_Map.hpp"
-    #define MAP(key) key                
-#endif
-// Memory_Map output file names
-#define MEMORY_MAP_OUTPUT_FILE_HPP  "./include/Memory_Map.hpp"
-#define MEMORY_MAP_OUTPUT_FILE_ASM  "./include/Memory_Map.asm"
+    #define MEMORY_MAP_OUTPUT_FILE_HPP  "./include/Memory_Map.hpp"
+    #define MEMORY_MAP_OUTPUT_FILE_ASM  "./include/Memory_Map.asm"
 
-// simple types for 8-bit archetecture 
-#ifndef Byte
-    #define Byte Uint8
-#endif
-#ifndef Word
-    #define Word Uint16
-#endif
-#ifndef DWord
-    #define DWord Uint32
-#endif
+    // simple types for 8-bit archetecture 
+    #ifndef Byte
+        #define Byte Uint8
+    #endif
+    #ifndef Word
+        #define Word Uint16
+    #endif
+    #ifndef DWord
+        #define DWord Uint32
+    #endif
 
-// common includes
-#include <SDL3/SDL.h>
-#include <string>
-#include <vector>
-#include <iostream>
-#include "clr.hpp"
+    // common includes
+    #include <SDL3/SDL.h>
+    #include <string>
+    #include <vector>
+    #include <iostream>
+    #include "clr.hpp"
+
+    // Create an appropriate MAP() macro
+    #if GENERATE_MEMORY_MAP == true
+        // use the unordered map
+        #define MAP_IMPL(key) Memory::Map(#key)                     
+        #define MAP(key) MAP_IMPL(key)
+    #else // use the enumeration
+        #include "Memory_Map.hpp"
+        #define MAP(key) key                
+    #endif  // END: GENERATE_MEMORY_MAP == true
 
 #endif  // __TYPES_HPP__
