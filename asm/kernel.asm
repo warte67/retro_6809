9a5f99655fb1f948ed1558b380ab5a81c861429f
warte67 <jayfaries@yahoo.com>
2025-01-20 17:12:40 -0800

    Kernel Work

--- asm/kernel.asm
+++ asm/kernel.asm
@@ -9,6 +9,14 @@
 ;* Original Author: Jay Faries (warte67)                                           
 ;*****************************
 
+sys			macro
+			swi2
+			fcb	\1
+			endm
+
+
+
+
 			INCLUDE "Memory_Map.asm"
 
 			org	SOFT_VECTORS_DEVICE
@@ -20,30 +28,63 @@
 			fdb	#0	; SOFT_IRQ             	
 			fdb	#0	; SOFT_SWI             	
 			fdb	#0	; SOFT_NMI             	
-			fdb	#0	; SOFT_RESET           	
+			fdb	#0	; SOFT_RESET   
 
-			org	HARD_RESET
+krnl_color_attribute	fcb	#$2a	; current color attribute
+
+
+			org	ROM_VECTS_DEVICE
+krnl_hard_vectors			
+			fdb	krnl_soft_exec_inf
+			fdb	krnl_soft_swi3_inf
+			fdb	krnl_swi2		; krnl_soft_swi2_inf
+			fdb	krnl_soft_firq_inf
+			fdb	krnl_soft_irq_inf
+			fdb	krnl_soft_swi_inf
+			fdb	krnl_soft_nmi_inf
 			fdb	start
+krnl_hard_vector_end
 
 ; ---------------------------------------------------------------------
-
 
 			org 	KERNEL_START
 
 start			bra	begin
 
-vers_label		fcn	"Kernel Version: "
-vers_number		fcn	"0.0.1"
+krnl_vers_label		fcn	"Kernel Version: "
+krnl_vers_number	fcn	"0.0.1"
+
+
+
+; default fake infinite loops... just to verify vector jumps
+krnl_soft_exec_inf	jmp	krnl_soft_exec_inf
+krnl_soft_swi3_inf	jmp	krnl_soft_swi3_inf
+krnl_soft_swi2_inf	jmp	krnl_soft_swi2_inf
+krnl_soft_firq_inf	jmp	krnl_soft_firq_inf
+krnl_soft_irq_inf	jmp	krnl_soft_irq_inf
+krnl_soft_swi_inf	jmp	krnl_soft_swi_inf
+krnl_soft_nmi_inf	jmp	krnl_soft_nmi_inf
+krnl_soft_reset_inf	jmp	krnl_soft_reset_inf
+
+
+; ---------------------------------------------------------------------
 
 begin			; warm reset?
 			ldd	SOFT_RESET
-			; cmpd	#0
+			cmpd	#0
 			beq	krnl_cold_reset
 			jmp	[SOFT_RESET]
 			
 krnl_cold_reset		; cold reset
+			ldx	#krnl_hard_vectors
+			ldu	#SOFT_VECTORS_DEVICE
+1			ldd	,x++
+			std	,u++
+			cmpx	#krnl_hard_vector_end
+			blt	1b
 			ldd	#krnl_warm_reset
 			std	SOFT_RESET
+
 			; ...
 			; clear out system memory
 krnl_clear_mem 		ldx	#SYSTEM_MEMORY_DEVICE
@@ -53,6 +94,8 @@ krnl_clear_mem 		ldx	#SYSTEM_MEMORY_DEVICE
 			std	,x++
 			cmpx	#KERNEL_ROM_DEVICE
 			bne	1b
+
+
 			; ...
 krnl_warm_reset		; common start up code
 			;
@@ -60,10 +103,35 @@ krnl_warm_reset		; common start up code
 			lds	#SSTACK_TOP
 			; ...
 
+			lda	#$4B			; $4B = green on dk.green
+			sta	krnl_color_attribute
 
 
-; ----------------------  MAIN LOOP  --------------------------------------
+			; make a system call
+			sys	#$00
+
+krnl_warm_reset_inf	jmp 	krnl_warm_reset_inf			
+
+
+
+; ----------------------  MAIN STUFF  --------------------------------------
+
+
+krnl_swi2_clear_screen
+			lda	krnl_color_attribute
+			ldb	#'A'
+
+			;ldd 	#$23fe
+
+			ldx	#VIDEO_START
+1			std	,x++
+			cmpx	GPU_VIDEO_MAX
+			blt	1b			
+			rts
+
+
 			; cycle video memory
+krnl_swi2_random_garbage			
 			ldd	#$0100
 1			ldx	#VIDEO_START		
 			addd	#1
@@ -75,3 +143,29 @@ krnl_warm_reset		; common start up code
 
 
 
+
+*** krnl_swi2 *******************************************************************
+** 
+** System Call Handler: 
+**	References the byte immediately following the SWI2 instruction and
+** 	dispatches to the appropriate system call based on that value.
+**
+**
+**********************************************************************************
+krnl_swi2_call_vector	fdb	krnl_swi2_clear_screen		; $00 clear screen
+			fdb	krnl_swi2_random_garbage	; $01 random garbage
+krnl_swi2_call_end				
+
+
+krnl_swi2		; increment the return address on the stack past the command byte
+			ldu	$000a,s
+			ldb	0,u
+			leau	1,u
+			stu	$000a,s
+			ldx	#krnl_swi2_call_vector
+			lslb
+			leax	b,x
+			cmpx	#krnl_swi2_call_end
+			bge	krnl_swi2_done
+			jsr	[,x]
+krnl_swi2_done		rti
