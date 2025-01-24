1e895861877863f01434e7f916316d795484036b
warte67 <jayfaries@yahoo.com>
2025-01-24 14:03:34 -0800

    Update GPU.hpp

--- include/GPU.hpp
+++ include/GPU.hpp
@@ -95,60 +95,44 @@ private: // PRIVATE MEMBERS
     // GPU_MODE
     Word _gpu_mode = 0b1011'1000'1011'0000; // (Word) Display Mode:
     // GPU_MODE_MSB                     // (Byte) Display Mode Most Significant Byte
-
                                         //    - bits 15 (7)  = Extended Display Enable
                                         //                 0: Disabled
                                         //                 1: Enabled
-
                                         //    - bit 14 (6)  = (reserved)
-
                                         //    - bits 12-13 (4-5) = Extended Color Depth:
                                         //                  00: 2-Colors
                                         //                  01: 4-Colors
                                         //                  10: 16-Colors
                                         //                  11: 256-Colors
-
                                         //    - bit 11 (3)   = Extended Rendering Mode:
                                         //                  0: Tilemap Display
                                         //                  1: Bitmap Display
-
                                         //    - bits 10 (2)  = Emulation Screen Mode
                                         //                 0: Windowed
                                         //                 1: Fullscreen
-
                                         //    - bit 9 (1)   = VSync Enable
                                         //                 0: Disabled
                                         //                 1: Enabled
-
                                         //    - bit 8 (0)   = Presentation
                                         //                 0: Overscan (Stretch)
                                         //                 1: Letterbox
-
                                         // -------------------------------------------------
     // GPU_MODE_LSB                     // (Byte) Display Mode Least Significant Byte
-
                                         //    - bit 7   = Standard Display Enable
                                         //                 0: Disabled
                                         //                 1: Enabled
-
                                         //    - bit 6    = (reserved)
-
                                         //    - bits 4-5 = Standard Bitmap Color Depth:
                                         //                  00: 2-Colors
                                         //                  01: 4-Colors
                                         //                  10: 16-Colors
                                         //                  11: 256-Colors
-
                                         //    - bit 3    = Standard Bitmap:
                                         //                  0: Text Display
                                         //                  1: Bitmap Display
-
                                         //    - bit 2    = 0: 320/256 width,  1: 160/128 width
                                         //    - bit 1    = 0: 200/160 height, 1: 160/80 height
                                         //    - bit 0    = Base Resolution: 0:320x200, 1:256x160
-
-
-
 
     // GPU_VIDEO_MAX
     Word _gpu_video_max = 0x23FF;       // (Word) Video Buffer Maximum (Read Only)
